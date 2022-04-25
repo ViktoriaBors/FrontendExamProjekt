1f45e2769731756311c0e969d311bd5ec8a7ea75
Viktoria Bors-Pajuste <viktoria.o.bors@gmail.com>
2022-04-25 14:29:40 +0200

    Menucard
    Focus keyup click events

--- menuScript.js
+++ menuScript.js
@@ -1,11 +1,19 @@
 let cardDrinks = document.querySelector('.cardMenuDrinks');
 let cardSnacks = document.querySelector('.cardMenuSnacks');
 
-//using focus instead of click - make sure that can be rached with keyboard
-cardDrinks.addEventListener('focus', function () {
+//using click and keyup to make sure that menu can be reached with keyboard too
+
+cardSnacks.addEventListener('click', function () {
+    cardSnacks.classList.toggle('is-flipped');
+});
+cardSnacks.addEventListener('keyup', function () {
+    cardSnacks.classList.toggle('is-flipped');
+});
+
+cardDrinks.addEventListener('click', function () {
     cardDrinks.classList.toggle('is-flipped');
 });
 
-cardSnacks.addEventListener('focus', function () {
-    cardSnacks.classList.toggle('is-flipped');
+cardDrinks.addEventListener('keyup', function () {
+    cardDrinks.classList.toggle('is-flipped');
 });
